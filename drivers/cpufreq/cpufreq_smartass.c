8cbae47488ab95c4dcd3ad6da642b2aa74d8a04a
Your Name <getitnowmarketing@gmail.com>
2011-01-19 00:31:18 -0500

    tune smarta gov

--- drivers/cpufreq/cpufreq_smartass.c
+++ drivers/cpufreq/cpufreq_smartass.c
@@ -80,14 +80,14 @@ static unsigned int up_min_freq;
  * to minimize wakeup issues.
  * Set sleep_max_freq=0 to disable this behavior.
  */
-#define DEFAULT_SLEEP_MAX_FREQ CONFIG_MSM_CPU_FREQ_ONDEMAND_MIN
+#define DEFAULT_SLEEP_MAX_FREQ 320000
 static unsigned int sleep_max_freq;
 
 /*
  * The frequency to set when waking up from sleep.
  * When sleep_max_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ CONFIG_MSM_CPU_FREQ_ONDEMAND_MAX
+#define DEFAULT_SLEEP_WAKEUP_FREQ 480000
 static unsigned int sleep_wakeup_freq;
 
 /*
@@ -108,13 +108,13 @@ static unsigned int sample_rate_jiffies;
  * Freqeuncy delta when ramping up.
  * zero disables causes to always jump straight to max frequency.
  */
-#define DEFAULT_RAMP_UP_STEP 352000
+#define DEFAULT_RAMP_UP_STEP 256000
 static unsigned int ramp_up_step;
 
 /*
  * Max freqeuncy delta when ramping down. zero disables.
  */
-#define DEFAULT_MAX_RAMP_DOWN 352000
+#define DEFAULT_MAX_RAMP_DOWN 256000
 static unsigned int max_ramp_down;
 
 /*
