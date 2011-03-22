441e719d9927a91a767db6d73887e693d9fdc8bf
Hugh Dickins <hughd@google.com>
2011-03-22 16:33:43 -0700

    shmem: let shared anonymous be nonlinear again
    commit bee4c36a5cf5c9f63ce1d7372aa62045fbd16d47 upstream.
    
    Up to 2.6.22, you could use remap_file_pages(2) on a tmpfs file or a
    shared mapping of /dev/zero or a shared anonymous mapping.  In 2.6.23 we
    disabled it by default, but set VM_CAN_NONLINEAR to enable it on safe
    mappings.  We made sure to set it in shmem_mmap() for tmpfs files, but
    missed it in shmem_zero_setup() for the others.  Fix that at last.
    
    Reported-by: Kenny Simpson <theonetruekenny@yahoo.com>
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2680,6 +2680,7 @@ void shmem_set_file(struct vm_area_struct *vma, struct file *file)
 		fput(vma->vm_file);
 	vma->vm_file = file;
 	vma->vm_ops = &shmem_vm_ops;
+	vma->vm_flags |= VM_CAN_NONLINEAR;
 }
 
 /**
@@ -2694,6 +2695,8 @@ int shmem_zero_setup(struct vm_area_struct *vma)
 	file = shmem_file_setup("dev/zero", size, vma->vm_flags);
 	if (IS_ERR(file))
 		return PTR_ERR(file);
+
 	shmem_set_file(vma, file);
+
 	return 0;
 }
