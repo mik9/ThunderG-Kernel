bf92dcd9751ea9406a78d1555f7cffed05be5bad
Your Name <getitnowmarketing@gmail.com>
2011-05-01 23:20:09 -0400

    update overclocking

--- arch/arm/mach-msm/acpuclock.c
+++ arch/arm/mach-msm/acpuclock.c
@@ -412,10 +412,11 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s) {
 	a11_div = hunt_s->a11clk_src_div;
 
 	if(hunt_s->a11clk_khz>600000) {
-					a11_div=0;
-					writel(hunt_s->a11clk_khz/19200, PLLn_L_VAL(0));
-					udelay(50);
-			}
+			a11_div=0;
+			writel(hunt_s->a11clk_khz/19200, PLLn_L_VAL(0));
+			cpu_relax();
+			udelay(50);
+		}
 
 
 	/*
@@ -431,11 +432,9 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s) {
 	/* Program clock source and divider */
 	reg_clkctl = readl(A11S_CLK_CNTL_ADDR);
 	reg_clkctl &= ~(0xFF << (8 * src_sel));
-	//OC HACK reg_clkctl |= hunt_s->a11clk_src_sel << (4 + 8 * src_sel);
 	reg_clkctl |=a11_div;
 	reg_clkctl |= hunt_s->a11clk_src_sel << (4 + 8 * src_sel);
 	
-	//OC HACK reg_clkctl |= hunt_s->a11clk_src_div << (0 + 8 * src_sel);
 	reg_clkctl |=a11_div;
 	reg_clkctl |= hunt_s->a11clk_src_div << (0 + 8 * src_sel);
 	writel(reg_clkctl, A11S_CLK_CNTL_ADDR);
@@ -444,6 +443,14 @@ static void acpuclk_set_div(const struct clkctl_acpu_speed *hunt_s) {
 	reg_clksel ^= 1;
 	writel(reg_clksel, A11S_CLK_SEL_ADDR);
 
+	// Recover from overclocking
+	if(hunt_s->pll==0 && hunt_s->a11clk_khz<=600000) {
+		// Restore the speed of PLL0
+		writel(50, PLLn_L_VAL(0));
+		cpu_relax();
+		udelay(50);
+	}
+	
 	/*
 	 * If the new clock divider is lower than the previous, then
 	 * program the divider after switching the clock
